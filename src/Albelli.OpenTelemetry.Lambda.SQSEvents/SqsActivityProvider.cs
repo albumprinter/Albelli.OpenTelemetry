145e91a6cd8cae6d65a0c1d474e497838a28bd2b
Kirill Ivanov <kirill.ivanov@albelli.com>
2021-01-25 13:43:10 +0100

    Implemented SQS

---
+++ src/Albelli.OpenTelemetry.Lambda.SQSEvents/SqsActivityProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Albelli.OpenTelemetry.Core;
+using Amazon.Lambda.SQSEvents;
+using JetBrains.Annotations;
+
+namespace Albelli.OpenTelemetry.Lambda.SQSEvents
+{
+    public class SqsActivityProvider
+    {
+        public static readonly ActivitySource Source = new ActivitySource("Albelli.OpenTelemetry.SQS");
+
+        [CanBeNull]
+        public Activity Start(SQSEvent.SQSMessage message)
+        {
+            var traceParent = message.ExtractStringAttribute(OpenTelemetryKeys.TraceParent);
+            var traceState = message.ExtractStringAttribute(OpenTelemetryKeys.TraceState);
+            var parentContext = ActivityContext.Parse(traceParent, traceState);
+
+            var activity = Source.StartActivity("SqsRequest", ActivityKind.Consumer, parentContext);
+
+            activity?.AddTag("sqs.event.source", message.EventSource);
+            activity?.AddTag("sqs.event.source.arn", message.EventSourceArn);
+            activity?.AddTag("sqs.message.id", message.MessageId);
+            activity?.AddTag("sqs.aws.region", message.AwsRegion);
+
+            return activity;
+        }
+    }
+}
---
+++ src/Albelli.OpenTelemetry.Lambda.SQSEvents/SqsMessageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.SQSEvents;
+
+namespace Albelli.OpenTelemetry.Lambda.SQSEvents
+{
+    public static class SqsMessageExtensions
+    {
+        public static string ExtractStringAttribute(this SQSEvent.SQSMessage message, string key)
+        {
+            if (message?.MessageAttributes != null && message.MessageAttributes.TryGetValue(key, out var attribute))
+            {
+                return attribute?.StringValue;
+            }
+            return null;
+        }
+    }
+}
