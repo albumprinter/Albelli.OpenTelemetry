c4db1f736ffd68ac8f3ed2e8bf3f570055ba3b8c
Kirill Ivanov <kirill.ivanov@albelli.com>
2021-04-22 09:21:42 +0200

    Added SDK init, so Propagators.DefaultTextMapPropagator will be initialized in static ctor

--- src/Albelli.OpenTelemetry.SNS/OpenTelemetrySns.cs
+++ src/Albelli.OpenTelemetry.SNS/OpenTelemetrySns.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Amazon.Runtime.Internal;
 using JetBrains.Annotations;
+using OpenTelemetry;
 using OpenTelemetry.Context.Propagation;
 
 namespace Albelli.OpenTelemetry.SNS
@@ -10,8 +11,12 @@ namespace Albelli.OpenTelemetry.SNS
     {
         public static readonly ActivitySource Source = new ActivitySource("Albelli.OpenTelemetry.SNS");
 
-        public static void ConfigureAwsOutgoingRequests(TextMapPropagator propagator)
+        public static void ConfigureAwsOutgoingRequests(TextMapPropagator propagator = null)
         {
+            //call to init Sdk static ctor
+            var _ = Sdk.SuppressInstrumentation;
+            propagator ??= Propagators.DefaultTextMapPropagator;
+
             RuntimePipelineCustomizerRegistry.Instance.Register(new OpenTelemetrySnsPipelineCustomizer(propagator));
         }
     }
