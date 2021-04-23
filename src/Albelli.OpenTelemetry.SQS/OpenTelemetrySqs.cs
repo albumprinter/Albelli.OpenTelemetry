709e0b601139b78094f8efb3739c280f42d8dfee
Kirill Ivanov <kirill.ivanov@albelli.com>
2021-04-23 14:12:39 +0200

    Now SQS sending depends on TextMapPropagator

--- src/Albelli.OpenTelemetry.SQS/OpenTelemetrySqs.cs
+++ src/Albelli.OpenTelemetry.SQS/OpenTelemetrySqs.cs
@@ -1,65 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System.Diagnostics;
 using Amazon.Runtime.Internal;
-using Amazon.SQS.Model;
 using JetBrains.Annotations;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
 
 namespace Albelli.OpenTelemetry.SQS
 {
     [PublicAPI]
     public static class OpenTelemetrySqs
     {
-        public static void ConfigureAwsOutgoingRequests()
+        public static readonly ActivitySource Source = new ActivitySource("Albelli.OpenTelemetry.SQS");
+
+        public static void ConfigureAwsOutgoingRequests(TextMapPropagator propagator = null)
         {
-            RuntimePipelineCustomizerRegistry.Instance.Register(new OpenTelemetrySqsPipelineCustomizer());
-        }
-
-        internal static void TryAdd(this IRequest request, string key, string value)
-        {
-            if (request == null || string.IsNullOrWhiteSpace(value))
-            {
-                return;
-            }
-
-            request.Headers[key] = value;
-        }
-
-        internal static void TryAdd(this SendMessageRequest sendMessageRequest, string key, string value)
-        {
-            sendMessageRequest.MessageAttributes ??= new Dictionary<string, MessageAttributeValue>();
-
-            if (!string.IsNullOrWhiteSpace(value) && !sendMessageRequest.MessageAttributes.ContainsKey(key))
-            {
-                sendMessageRequest.MessageAttributes[key] = new MessageAttributeValue
-                {
-                    DataType = "String",
-                    StringValue = value
-                };
-            }
-        }
-
-        internal static void TryAdd(this SendMessageBatchRequestEntry sendMessageBatchRequestEntry, string key, string value)
-        {
-            sendMessageBatchRequestEntry.MessageAttributes ??= new Dictionary<string, MessageAttributeValue>();
-
-            if (!string.IsNullOrWhiteSpace(value) && !sendMessageBatchRequestEntry.MessageAttributes.ContainsKey(key))
-            {
-                sendMessageBatchRequestEntry.MessageAttributes[key] = new MessageAttributeValue
-                {
-                    DataType = "String",
-                    StringValue = value
-                };
-            }
-        }
-
-        internal static void TryAdd(this ReceiveMessageRequest receiveMessageRequest, string key)
-        {
-            receiveMessageRequest.MessageAttributeNames ??= new List<string>();
-
-            if (!receiveMessageRequest.MessageAttributeNames.Contains(key) &&
-                !receiveMessageRequest.MessageAttributeNames.Contains("All"))
-            {
-                receiveMessageRequest.MessageAttributeNames.Add(key);
-            }
+            //call to init Sdk static ctor to initialize Propagators.DefaultTextMapPropagator properly
+            var _ = Sdk.SuppressInstrumentation;
+            propagator ??= Propagators.DefaultTextMapPropagator;
+            RuntimePipelineCustomizerRegistry.Instance.Register(new OpenTelemetrySqsPipelineCustomizer(propagator));
         }
     }
 }
